b24af8b9cb8cc6866e0b6bba9c6664d055cbad47
penscola <penscolashackletonfelix@gmail.com>
2023-03-11 00:15:27 +0300

    Created .c files

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks if a character is an alphabetic character.
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if the character is a letter, 0 otherwise.
+ */
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
