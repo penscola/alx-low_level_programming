f9f453f9f1d86d1c21e9b31f2d9a1f13f5b0d4d9
penscola <penscolashackletonfelix@gmail.com>
2023-03-13 21:23:21 +0300

    check the code for ALX School students

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Create a 2D array of integers
+ * @width: width input
+ * @height: height input
+ *
+ * Return: pointer to 2D array of integers or NULL on failure
+ */
+int **alloc_grid(int width, int height)
+
+{
+int **grid;
+int i, j;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+grid = malloc(sizeof(int *) * height);
+
+if (grid == NULL)
+return (NULL);
+
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(sizeof(int) * width);
+
+if (grid[i] == NULL)
+{
+for (; i >= 0; i--)
+free(grid[i]);
+
+free(grid);
+return (NULL);
+}
+}
+
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+}
+
+return (grid);
+}
+
