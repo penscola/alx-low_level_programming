726be54eebb4800b2737cef1257b8a53c70ffdb1
penscola <penscolashackletonfelix@gmail.com>
2023-03-21 00:18:13 +0300

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
+ * main - Prints the name of the current file
  *
  * Return: Always 0 (Success)
  */
-int ain(void)
+int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
-
