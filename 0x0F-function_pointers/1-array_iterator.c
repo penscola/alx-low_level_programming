89c1a6612b6da9a085812981aab8a8995fed761f
penscola <penscolashackletonfelix@gmail.com>
2023-03-22 12:16:39 +0300

    If you spend too much time thinking about a thing, you will  never get it done

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - Prints each array element on a new line
+ * @array: Array
+ * @size: How many elements to print
+ * @action: Pointer to print in regular or hex
+ *
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
+
