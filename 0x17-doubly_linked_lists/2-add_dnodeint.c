f71282c2ccd82849361dd4586c5b2d035c605f90
penscola <penscolashackletonfelix@gmail.com>
2023-06-15 15:16:29 +0300

    chore: Add node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint - add a node at the beginning
+ *@head: the head of the node
+ *@n : the int to add
+ *Return: The new node.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+    dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+    if (new_node == NULL)
+        return (NULL);
+    new_node->n = n;
+    new_node->prev = NULL;
+
+    if (*head == NULL)
+    {
+        new_node->next = NULL;
+        *head = new_node;
+        return (new_node);
+    }
+    else
+    {
+        new_node->next = *head;
+        (*head)->prev = new_node;
+        *head = new_node;
+        return (new_node);
+    }
+}
