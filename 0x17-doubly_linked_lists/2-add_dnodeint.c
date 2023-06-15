3f03cc7b0aea8ecd6a322629cf27edd8729d9124
penscola <penscolashackletonfelix@gmail.com>
2023-06-15 15:18:30 +0300

    updates

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -9,24 +9,24 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
-    if (new_node == NULL)
-        return (NULL);
-    new_node->n = n;
-    new_node->prev = NULL;
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->prev = NULL;
 
-    if (*head == NULL)
-    {
-        new_node->next = NULL;
-        *head = new_node;
-        return (new_node);
-    }
-    else
-    {
-        new_node->next = *head;
-        (*head)->prev = new_node;
-        *head = new_node;
-        return (new_node);
-    }
+if (*head == NULL)
+{
+new_node->next = NULL;
+*head = new_node;
+return (new_node);
+}
+else
+{
+new_node->next = *head;
+(*head)->prev = new_node;
+*head = new_node;
+return (new_node);
+}
 }
